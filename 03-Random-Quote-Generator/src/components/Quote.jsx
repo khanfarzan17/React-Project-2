05bb2739c642c2b90d3970dbb251d1aaf3142bc2
khanfarzan17 <116535467+khanfarzan17@users.noreply.github.com>
2024-07-30 09:59:45 +0530

    Improve Code

--- 03-Random-Quote-Generator/src/components/Quote.jsx
+++ 03-Random-Quote-Generator/src/components/Quote.jsx
@@ -1,20 +1,19 @@
-import React, { useState } from "react";
+import { useState, useCallback } from "react";
 import quotes from "../Data/data";
 import "../styles/Quote.css";
 
 const Quote = () => {
   const [quotee, setQuotee] = useState(quotes[0] || {});
 
-  const handleNext = () => {
+  const handleNext = useCallback(() => {
     const randomIndex = Math.floor(Math.random() * quotes.length);
-    const newQuo = quotes[randomIndex];
-    setQuotee(newQuo);
-  };
+    setQuotee(quotes[randomIndex]);
+  }, []);
 
   return (
     <div className="container">
       <h1 className="heading-1">{quotee.quote}</h1>
-      <h3 className="heading-2">Author :- {quotee.author}</h3>
+      <h3 className="heading-2">Author: {quotee.author}</h3>
       <button className="button" onClick={handleNext}>
         Next Quote
       </button>
